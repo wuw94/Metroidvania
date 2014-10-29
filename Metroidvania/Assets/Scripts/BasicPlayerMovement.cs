e75c2dda9ca07c6c3e23790b6e3378c4088ed6f8
Wesley <weslew1@uci.edu>
2014-10-29 15:11:12 -0700

    testing

---
+++ Metroidvania/Assets/Scripts/BasicPlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicPlayerMovement : GlobalVariables {
+	bool grounded = true;
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.tag == "ImpassableTopBottom")
+		{
+			grounded = true;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		TestStuff();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float x = rigidbody2D.velocity.x;
+		float y = rigidbody2D.velocity.y;
+		if(Input.GetKey(IN_RIGHT))
+		{
+			rigidbody2D.velocity = new Vector2(x+0.3f,y);
+		}
+		if(Input.GetKey(IN_LEFT))
+		{
+			rigidbody2D.velocity = new Vector2(x-0.3f,y);
+		}
+		if(Input.GetKey(IN_JUMP) && grounded)
+		{
+			rigidbody2D.velocity = new Vector2(x,y+10);
+			grounded = false;
+		}
+	}
+}
