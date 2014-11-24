b72e0687307ea37abe06a9888b4a662b0487769e
Azirly <JYLin5@gmail.com>
2014-11-23 16:15:12 -0800

    Bomb and Heatlh Script
    May have some errors as I am confused on what Collider is.

---
+++ Metroidvania/Assets/Scripts/Entity/Bomb.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+	public float Character_Health;
+	public float ticking_time;
+	public float power = 20.0f;
+	public float radius = 5.0f;
+	public Collider physics_detection;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				Vector2 explosion_position = transform.position;
+				ticking_time -= Time.deltaTime * 10f;
+				//Collider[] colliding_items = Physics.CheckSphere(explosionPos, radius);
+				if (ticking_time == 0)
+					physics_detection.rigidbody.AddExplosionForce (power, explosion_position, radius, 1.0F);
+					Character_Health = Character_Health - power;
+
+						}
+		}
---
+++ Metroidvania/Assets/Scripts/Entity/Health.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+	public int Character_Health;
+	// Use this for initialization
+	void Start () {
+		Character_Health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
