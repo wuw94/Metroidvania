813da55eb0a18995b9d0ce3b1d049c174d94bd5d
Azirly <JYLin5@gmail.com>
2014-12-04 19:32:08 -0800

    Instant Destructible Wall
    Wall will instantly explode, but it is now destructible

---
+++ Metroidvania/Assets/Scripts/Entity/Wall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : Immobile {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Action ();
+	}
+
+	public override void Action()
+	{
+		Destroy(gameObject);
+	}
+}
