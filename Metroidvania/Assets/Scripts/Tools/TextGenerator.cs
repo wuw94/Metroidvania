1fdedf9461c486fabbb7abb11b1809250b4ef521
Azirly <JYLin5@gmail.com>
2015-01-14 23:37:00 -0800

    Simple Text Editor #2, one w/ problems

--- Metroidvania/Assets/Scripts/Tools/TextGenerator.cs
+++ Metroidvania/Assets/Scripts/Tools/TextGenerator.cs
@@ -7,16 +7,17 @@ public class TextGenerator : MonoBehaviour {
 
 
 	public string funky_string;
-	public string funky_string2;
-	
+
+
 	void OnGUI () {
 		funky_string = GUI.TextArea (new Rect(0, 0, 200, 100), "Top-left");
-		GUI.Box (new Rect (0,0,100,50), "Top-left");
-		GUI.Box (new Rect (Screen.width - 100,0,100,50), "Top-right");
-		GUI.Box (new Rect (0,Screen.height - 50,100,50), "Bottom-left");
-		GUI.Box (new Rect (Screen.width - 100,Screen.height - 50,100,50), "Bottom-right");
-
+		if (GUI.changed) {
+			Debug.Log("First button was clicked");
+			funky_string = " ";
+			GUI.TextArea (new Rect (0, 0, 100, 50), "Top-left");
+			GUI.TextArea (new Rect (Screen.width - 100, 0, 100, 50), "Top-right");
+			GUI.TextArea (new Rect (0, Screen.height - 50, 100, 50), "Bottom-left");
+			GUI.TextArea (new Rect (Screen.width - 100, Screen.height - 50, 100, 50), "Bottom-right");
+		}
 	}
-
 }
-
